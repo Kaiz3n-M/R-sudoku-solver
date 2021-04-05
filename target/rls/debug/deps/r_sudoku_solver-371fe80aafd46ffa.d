78286557e4c4070739a91080dfcc8187126cabc7
kaiz3n-m <falletti.hackmar1@gmail.com>
2021-04-05 22:52:56 +0100

    add git ignore

--- target/rls/debug/deps/r_sudoku_solver-371fe80aafd46ffa.d
+++ target/rls/debug/deps/r_sudoku_solver-371fe80aafd46ffa.d
@@ -1,5 +1,5 @@
-/home/kaiz3n/Dev/Rust/r-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-371fe80aafd46ffa.rmeta: src/main.rs
+/home/kaiz3n/Dev/Rust/R-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-371fe80aafd46ffa.rmeta: src/main.rs
 
-/home/kaiz3n/Dev/Rust/r-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-371fe80aafd46ffa.d: src/main.rs
+/home/kaiz3n/Dev/Rust/R-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-371fe80aafd46ffa.d: src/main.rs
 
 src/main.rs:
--- target/rls/debug/deps/r_sudoku_solver-e799a3dc6e435186.d
+++ target/rls/debug/deps/r_sudoku_solver-e799a3dc6e435186.d
@@ -1,5 +1,5 @@
-/home/kaiz3n/Dev/Rust/r-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-e799a3dc6e435186.rmeta: src/main.rs
+/home/kaiz3n/Dev/Rust/R-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-e799a3dc6e435186.rmeta: src/main.rs
 
-/home/kaiz3n/Dev/Rust/r-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-e799a3dc6e435186.d: src/main.rs
+/home/kaiz3n/Dev/Rust/R-sudoku-solver/target/rls/debug/deps/r_sudoku_solver-e799a3dc6e435186.d: src/main.rs
 
 src/main.rs:
